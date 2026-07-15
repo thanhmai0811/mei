07e8f93412b5ce933b8738479a7764d39ffaf08a
thanhmai <124651620+thanhmai0811@users.noreply.github.com>
2026-07-15 14:57:32 +0700

    button.h

--- button.h
+++ button.h
@@ -1,79 +1,17 @@
-#define BUTTON_PIN 10
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-button
+ */
 
-bool lastButtonState = HIGH;   // luôn để HIGH nếu dùng INPUT_PULLUP
-
-void setup() {
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-}
-
-void loop() {
-  bool buttonState = digitalRead(BUTTON_PIN);
-
-  // (A) Phát hiện cạnh xuống = thời điểm VỪA nhấn
-  if (lastButtonState == HIGH && buttonState == LOW) {
-    delay(30); // (B) Debounce
-    if (digitalRead(BUTTON_PIN) == LOW) {
-
-      // (C) ----> CHỖ NÀY THAY BẰNG LOGIC RIÊNG CỦA TỪNG BÀI <----
-      xuLyKhiNhanNut();
-
-      // (D) Chờ nhả nút, chặn đếm lặp khi giữ
-      while (digitalRead(BUTTON_PIN) == LOW) {
-        delay(10);
-      }
-    }
-  }
-
-  lastButtonState = buttonState; // (E) Cập nhật trạng thái
-}
-
-//=========================HAM NANG CAP (MILLIS)==================================//
-#define BUTTON_PIN 10
-#define DEBOUNCE_DELAY 30   // thời gian chống dội (ms)
-
-bool lastRawState = HIGH;       // trạng thái đọc thô lần trước
-bool stableState = HIGH;        // trạng thái đã xác nhận ổn định
-unsigned long lastDebounceTime = 0;
-
-void setup() {
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-}
-
-bool kiemTraNutVuaNhan() {
-  bool raw = digitalRead(BUTTON_PIN);
-  bool result = false;
-
-  // Nếu tín hiệu thô thay đổi -> reset lại mốc thời gian đếm debounce
-  if (raw != lastRawState) {
-    lastDebounceTime = millis();
-  }
-
-  // Nếu tín hiệu đã đứng yên đủ lâu (không rung nữa)
-  if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
-    // Nếu trạng thái ổn định thực sự thay đổi so với lần xác nhận trước
-    if (raw != stableState) {
-      stableState = raw;
-      if (stableState == LOW) {
-        result = true; // phát hiện 1 lần nhấn hợp lệ
-      }
-    }
-  }
-
-  lastRawState = raw;
-  return result;
-}
-
-void loop() {
-  if (kiemTraNutVuaNhan()) {
-    // logic xử lý khi nhấn nút ở đây
-  }
-//==========================================================================================//
 // constants won't change. They're used here to set pin numbers:
-const int BUTTON_PIN = 7; // the number of the pushbutton pin
+const int BUTTON_PIN = 7;       // the number of the pushbutton pin
 
 // Variables will change:
-int lastState = HIGH; // the previous state from the input pin
-int currentState;    // the current reading from the input pin
+int lastState = LOW;  // the previous state from the input pin
+int currentState;                // the current reading from the input pin
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -87,11 +25,11 @@ void loop() {
   // read the state of the switch/button:
   currentState = digitalRead(BUTTON_PIN);
 
-  if(lastState == LOW && currentState == HIGH)
-    Serial.println("The state changed from LOW to HIGH");
+  if(lastState == HIGH && currentState == LOW)
+    Serial.println("The button is pressed");
+  else if(lastState == LOW && currentState == HIGH)
+    Serial.println("The button is released");
 
-  // save the last state
+  // save the the last state
   lastState = currentState;
-}
-
 }
